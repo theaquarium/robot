b0a126633df79e1dfa65e17b52a8f6f2ec974bc6
aquarium <aquariumpain@gmail.com>
2018-06-28 21:50:10 -0400

    Add all directions

--- robot.py
+++ robot.py
@@ -6,49 +6,50 @@ class Robot:
         self.motor_front_right = Motor(10, 8, 'front right')
         self.motor_rear_left = Motor(18, 22, 'rear left')
         self.motor_rear_right = Motor(24, 26, 'rear right')
-    
+        self.pwr = .05
+        self.pwr_turn = .001
+
     def forward(self):
         print('*** Going forward!')
 
-        pwr = .05
-        self.motor_front_left.set_power(pwr)
-        self.motor_front_right.set_power(pwr)
-        
-        self.motor_rear_left.set_power(pwr)
-        self.motor_rear_right.set_power(pwr)
-    
+        self.motor_front_left.set_power(self.pwr)
+        self.motor_front_right.set_power(self.pwr)
+
+        self.motor_rear_left.set_power(self.pwr)
+        self.motor_rear_right.set_power(self.pwr)
+
     def back(self):
         print('*** Going back!')
 
-        self.motor_front_left.turn_anticlockwise()
-        self.motor_front_right.turn_clockwise()
-        
-        self.motor_rear_left.turn_anticlockwise()
-        self.motor_rear_right.turn_clockwise()
-    
+        self.motor_front_left.set_power(-self.pwr)
+        self.motor_front_right.set_power(-self.pwr)
+
+        self.motor_rear_left.set_power(-self.pwr)
+        self.motor_rear_right.set_power(-self.pwr)
+
     def left(self):
         print('*** Turning left!')
 
-        self.motor_front_left.turn_anticlockwise()
-        self.motor_front_right.turn_anticlockwise()
-        
-        self.motor_rear_left.turn_anticlockwise()
-        self.motor_rear_right.turn_anticlockwise()
-    
+        self.motor_front_left.set_power(-self.pwr_turn)
+        self.motor_front_right.set_power(self.pwr_turn)
+
+        self.motor_rear_left.set_power(-self.pwr_turn)
+        self.motor_rear_right.set_power(self.pwr_turn)
+
     def right(self):
         print('*** Turning right!')
 
-        self.motor_front_left.turn_clockwise()
-        self.motor_front_right.turn_clockwise()
-        
-        self.motor_rear_left.turn_clockwise()
-        self.motor_rear_right.turn_clockwise()
-    
+        self.motor_front_left.set_power(self.pwr_turn)
+        self.motor_front_right.set_power(-self.pwr_turn)
+
+        self.motor_rear_left.set_power(self.pwr_turn)
+        self.motor_rear_right.set_power(-self.pwr_turn)
+
     def stop(self):
         print('*** Stopping!')
 
         self.motor_front_left.set_power(0)
         self.motor_front_right.set_power(0)
-        
+
         self.motor_rear_left.set_power(0)
         self.motor_rear_right.set_power(0)
