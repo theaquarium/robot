9da93931ddaba14276e21788a2611b58f5165a6f
aquarium <aquariumpain@gmail.com>
2018-06-29 16:58:10 -0400

    New UI

---
+++ static/js/main.js
@@ -0,0 +1,130 @@
+// Global Variables
+const directionMap = {
+    'up': 'forward',
+    'down': 'back',
+    'left': 'left',
+    'right': 'right',
+}
+let manager;
+let lastCommand;
+
+// Send Command Function
+function command(cmd) {
+    if (cmd && cmd != lastCommand) {
+        console.log(cmd);
+        const xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('GET', '/api/command/' + cmd, true);
+        xmlHttp.send(null);
+        lastCommand = cmd;
+    }
+}
+
+// Add Active Class To Button
+function activeButton(buttonClass) {
+    document.querySelector('.' + buttonClass).classList.add('button-active');
+}
+
+// Button Listeners
+function buttonMouseDown() {
+    let direction;
+    const classes = this.classList.values();
+    for(let className of classes) {
+        if (className != 'button') direction = className;
+    }
+    command(directionMap[direction]);
+}
+
+function buttonMouseUp() {
+    command('stop');
+}
+
+function buttonKeyDown(event) {
+    switch(event.key) {
+        case 'ArrowUp':
+            command('forward');
+            activeButton('up');
+            break;
+        case 'ArrowDown':
+            command('back');
+            activeButton('down');
+            break;
+        case 'ArrowLeft':
+            command('left');
+            activeButton('left');
+            break;
+        case 'ArrowRight':
+            command('right');
+            activeButton('right');
+            break;
+    }
+}
+
+function buttonKeyUp() {
+    command('stop');
+    document.querySelectorAll('.button-active').forEach(el => {
+        el.classList.remove('button-active');
+    });
+}
+
+// Joystick Functions
+function activateJoystick() {
+    manager = nipplejs.create({
+        mode: 'static',
+        zone: document.querySelector('.joystick'),
+        position: {left: '50%', top: '50%'},
+        restOpacity: 0.8,
+    });
+    
+    manager.on('dir', (event, data) => {
+        const direction = data.direction.angle;
+        command(directionMap[direction]);
+    }).on('end', event => {
+        command('stop');
+    });
+}
+
+function deactivateJoystick() {
+    manager.destroy();
+}
+
+// Button Functions
+function activateButtons() {
+    document.querySelectorAll('.button').forEach(button => {
+        button.addEventListener('mousedown', buttonMouseDown);
+        button.addEventListener('mouseup', buttonMouseUp);
+    });
+    document.addEventListener('keydown', buttonKeyDown);
+    document.addEventListener('keyup', buttonKeyUp);
+}
+
+function deactivateButtons() {
+    document.querySelectorAll('.button').forEach(button => {
+        button.removeEventListener('mousedown', buttonMouseDown);
+        button.removeEventListener('mouseup', buttonMouseUp);
+    });
+    document.removeEventListener('keydown', buttonKeyDown);
+    document.removeEventListener('keyup', buttonKeyUp);
+}
+
+// Toggle Switch Listener
+document.querySelector('input[name=switch-input]').addEventListener('change', function() {
+    if (this.checked) {
+        // Remove Buttons
+        deactivateButtons();
+        document.querySelector('.buttons').classList.add('hidden');
+        // Add Joystick
+        document.querySelector('.joystick').classList.remove('hidden');
+        activateJoystick();
+    }
+    else {
+        // Remove Joystick
+        deactivateJoystick();
+        document.querySelector('.joystick').classList.add('hidden');
+        // Add Buttons
+        document.querySelector('.buttons').classList.remove('hidden');
+        activateButtons();
+    }
+});
+
+// Activate Buttons
+activateButtons();
